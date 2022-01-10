4b0472328f4cf3308082d2d4b52033c757792404
FarahMourad <51189763+FarahMourad@users.noreply.github.com>
2022-01-10 18:57:40 +0200

    Update SalesController.php

--- app/Http/Controllers/SalesController.php
+++ app/Http/Controllers/SalesController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use App\Models\Sales;
 use Illuminate\Http\Request;
 use \Illuminate\Http\JsonResponse;
@@ -12,7 +13,10 @@ class SalesController
     public function buyBook(Request $request){
         $sale = new Sales();
         $sale->ISBN = $request->isbn;
+        $price = Book::where('ISBN', $request->isbn)->first();
         $sale->user_id = auth()->user()->id;
         $sale->copies = $request->copies;
+        $sale->total_price = $price * $request->copies;
+        redirect()->back();
     }
 }
